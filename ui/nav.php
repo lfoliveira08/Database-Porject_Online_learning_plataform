72f5e2c0e0213f375ef61265c52a91e034996655
lfoliveira08 <lfoliveira609@gmail.com>
2026-04-09 16:58:22 -0500

    Add CSS styles and navigation bar

---
+++ ui/nav.php
@@ -0,0 +1,13 @@
+<!-- nav.php -->
+<header>
+    <nav>
+        <a href="reports.php" class="logo">TechLearn Platform</a>
+        <ul>
+            <li><a href="register_student.php">Register Student</a></li>
+            <li><a href="enroll_student.php">Enroll Student</a></li>
+            <li><a href="enter_grade.php">Enter Grade</a></li>
+            <li><a href="reports.php">Reports</a></li>
+            <li><a href="instructor_dashboard.php">Instructor Dashboard</a></li>
+        </ul>
+    </nav>
+</header>
